408579f3803adda216dbec4a6ebacde05abeabc6
Rubear <rubenslabbert@gmail.com>
2013-07-13 15:19:09 +1000

    Plurple

--- reddit.h
+++ reddit.h
@@ -8,12 +8,7 @@
 #include <curl/curl.h>
  
 #define REDDIT_URL_BASE_LENGTH 21 
-<<<<<<< HEAD
-#define SIZEOFELEM(x)  (sizeof(x) / sizeof(x[0]))
-
-=======
  
->>>>>>> 4f71652bef00b52b08fea1eff463ca00cfcc7686
 struct MemoryStruct {
   	char *memory;
   	size_t size;
@@ -34,13 +29,8 @@ struct comments {
 	char * votes;
 };
 
-<<<<<<< HEAD
 void redditGetThread(char * postid, struct comments * commentList, int * commentCount);
 void redditGetSubreddit(char * sub, char * sorting, struct post * postList, int * postCount);
-=======
-void redditGetThread(char * postid, struct comments * commentList);
-void redditGetSubreddit(char * sub, char * sorting, struct post * postList);
->>>>>>> 4f71652bef00b52b08fea1eff463ca00cfcc7686
 char *ask_for_subreddit();
 void showSubreddit(char *subreddit);
 void cleanup();
