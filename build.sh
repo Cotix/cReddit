5374ee936d9f253f109a77ac726d85d85f4b6fd5
comfortablejohn <comfortablejohn@gmail.com>
2013-07-18 22:00:47 +0800

    Changed build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-if [ ! -d "./build"]; then
+DIR="./build"
+if [ ! -d "$DIR" ]; then
+  echo "Creating 'build' directory..."
   mkdir ./build
 fi
 gcc -g -o ./build/cReddit `curl-config --cflags` main.c reddit.c jsmn.c -lncurses `curl-config --libs`
