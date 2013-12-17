cae71b67e4fd5c5c5f0ad8b4f8e66dd5f2ea305c
Matt Kilgore <mattkilgore12@gmail.com>
2013-12-17 14:25:00 -0500

    creddit: Add 'alloc_sprintf' function.
    This patch adds a 'alloc_sprintf' function, which take the same
    arguments as sprintf and returns a buffer allocated to the exact size
    neeed by that sprintf. The buffer needs to be freed by using free().

--- src/global.c
+++ src/global.c
@@ -1,6 +1,34 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "global.h"
 
 FILE *debugFile;
+
+static char *valloc_sprintf (const char *format, va_list lst)
+{
+    size_t size = vsnprintf(NULL, 0, format, lst) + 1;
+    char *buf = malloc(size);
+
+    if (!buf)
+        return NULL;
+
+    buf[0] = '\0';
+
+    vsnprintf(buf, size, format, lst);
+    return buf;
+}
+
+char *alloc_sprintf (const char *format, ...)
+{
+    char *ret;
+    va_list lst;
+
+    va_start(lst, format);
+    ret = valloc_sprintf(format, lst);
+    va_end(lst);
+
+    return ret;
+}
+
